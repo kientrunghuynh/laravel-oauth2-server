4a5f5de6ed2f12402dce880fd071a6b22c568c90
Trung Huynh <thy@tapptic.com>
2016-03-07 15:45:46 +0700

    implement oAuth2.0 server

---
+++ app/Http/routes.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', ['middleware' => 'oauth', function () {
+    return view('welcome');
+}]);
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
+
+/*
+|--------------------------------------------------------------------------
+| oAuth2 Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+Route::post('auth/access_token', [
+    'as'   => 'access_token',
+    'uses' => 'OauthController@accessToken'
+]);
